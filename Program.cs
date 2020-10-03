ac863e5d2bb65998881cc77e15c9b440f524f8c0
Vitoria <vitoriahellen3004@gmail.com>
2020-10-03 20:03:19 -0300

    Converta uma temperatura digitada pelo usuário em °F para °C.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace grausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorc;
+            double valorf;
+
+            Console.WriteLine("Coversão de temperatura");
+            Console.Write("°F: ");
+            valorf = Convert.ToDouble(Console.ReadLine());
+            valorc = (valorf - 32)/1.8;
+            Console.WriteLine($"O {valorf} equivalem a {valorc}° ");
+
+        }
+    }
+}
